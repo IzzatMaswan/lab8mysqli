37b0cd3c5b16e89804433d32f061202c611d72c3
IzzatMaswan <2020866462@student.uitm.edu.my>
2022-06-08 01:02:02 +0800

    first commit

---
+++ add_user.php
@@ -0,0 +1,12 @@
+<?php
+include 'conn.php';
+
+$sql="insert into user (first_name, last_name, email, pass_word)
+value ($_post(firstname)','$_post (lastname)','$_post(email)','
+$_post(password)','$_post(reg_date)')";
+    if (!mysqli_query($conn, $sql)){
+        die('Error: '.mysqli_error());
+    }
+    echo "1 record successfully added...";
+    
+?>
