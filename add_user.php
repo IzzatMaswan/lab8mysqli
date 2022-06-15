6d84f04f54a6a1700f7f53b404f4ffead89bdfb2
IzzatMaswan <2020866462@student.uitm.edu.my>
2022-06-15 22:11:32 +0800

    fix2

--- add_user.php
+++ add_user.php
@@ -2,8 +2,7 @@
 include 'conn.php';
 
 $sql="insert into user (first_name, last_name, email, pass_word)
-value ($_post(firstname)','$_post (lastname)','$_post(email)','
-$_post(password)','$_post(reg_date)')";
+value ($_post[firstname]','$_post [lastname]','$_post[email]','$_post[password]','$_post[reg_date]')";
     if (!mysqli_query($conn, $sql)){
         die('Error: '.mysqli_error());
     }
